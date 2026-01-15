672a933ae44bc30403468db35959bbaf4c0eab7c
Bubbles <nicolas.champagne03@gmail.com>
2026-01-15 09:42:07 +0100

    initial commit

---
+++ src/main/java/me/bubbles/Main.java
@@ -0,0 +1,18 @@
+package me.bubbles;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+}
